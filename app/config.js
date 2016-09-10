cb709276bad1014f5c9ae6d6babdcb27522c1fa3
Fredrik Lack <fredrik@nody.se>
2016-09-10 10:20:48 +0200

    Updating docker implementation

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
-  ELASTIC_HOST: process.env.ELASTIC_HOST,
-  ELASTIC_LOG: process.env.ELASTIC_LOG || 'trace',
+  ELASTIC_HOST: 'bolagetio_elasticsearch:9200',
+  ELASTIC_LOG: process.env.ELASTIC_LOG,
   ELASTIC_INDEX: process.env.ELASTIC_INDEX,
   PORT: 3000
 }
