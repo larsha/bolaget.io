21865b739574c45708f30bbb52ffd252bb2391ea
Fredrik Lack <fredrik@nody.se>
2016-10-03 20:43:13 +0200

    Adding first test, adding travis

---
+++ app/lib/test/elastic.spec.js
@@ -0,0 +1,9 @@
+import { expect, assert } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import Elastic from '../elastic'
+
+describe('Elastic', () => {
+  it('should exist', () => {
+    expect(Elastic).to.exist
+  })
+})
