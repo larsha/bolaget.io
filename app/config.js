dc6935c3251f37725e3722d648e1499ed3f6f375
Fredrik Lack <fredrik@nody.se>
2016-10-03 21:24:06 +0200

    Trying docker in travis

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 const config = {
-  ELASTIC_HOST: process.env.ELASTIC_HOST || 'http://bolagetio_elasticsearch:9200',
+  ELASTIC_HOST: 'http://bolagetio_elasticsearch:9200',
   ELASTIC_LOG: process.env.ELASTIC_LOG,
   ELASTIC_INDEX: process.env.ELASTIC_INDEX,
   PORT: 3000
