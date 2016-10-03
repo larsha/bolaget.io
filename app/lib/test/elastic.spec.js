e33ed6b88328c7204d0b711997ae684d3c71a4d9
Fredrik Lack <fredrik@nody.se>
2016-10-03 20:59:54 +0200

    Adding tests for create/delete index

--- app/lib/test/elastic.spec.js
+++ app/lib/test/elastic.spec.js
@@ -1,9 +1,31 @@
 import { expect, assert } from 'chai'
+import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import Elastic from '../elastic'
+
+chai.should()
 
 describe('Elastic', () => {
   it('should exist', () => {
     expect(Elastic).to.exist
+  })
+
+  describe('indexing()', () => {
+    const index = 'test'
+
+    after(async () => {
+      const promise = await Elastic.deleteIndex(index)
+      return promise.should.be.fulfilled
+    })
+
+    it('delete index', async () => {
+      const promise = await Elastic.deleteIndex(index)
+      return promise.should.be.fulfilled
+    })
+
+    it('create new index', async () => {
+      const promise = await Elastic.createIndex(index)
+      return promise.should.be.fulfilled
+    })
   })
 })
