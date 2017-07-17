e4e74c4a16eb7f965761f98acbf053bfb786551e
Fredrik Lack <fredrik@brynn.se>
2017-07-17 12:28:24 +0200

    Minor update to k8s config files

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ const config = {
   ELASTIC_HOST: process.env.ES_HOST || 'elastic:9200',
   ELASTIC_LOG: process.env.ELASTIC_LOG,
   ELASTIC_INDEX: process.env.ELASTIC_INDEX || 'bolagetio_dev',
-  PORT: 3000
+  PORT: process.env.PORT || 3000
 }
 
 export default config
