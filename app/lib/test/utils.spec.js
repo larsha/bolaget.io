9801cf042bcd80eca632e580b487e7dd9e8d0835
Fredrik Lack <fredrik@brynn.se>
2017-09-09 14:27:08 +0200

    Bumping node, fixing coveralls, minor refactoring

--- app/lib/test/utils.spec.js
+++ app/lib/test/utils.spec.js
@@ -1,13 +1,17 @@
 import { expect } from 'chai'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import { stringToBool } from '../utils'
+import { stringToBool, listToArray } from '../utils'
 
 chai.use(chaiAsPromised)
 chai.should()
 
 describe('Utils', function () {
-  it('string to bool', function () {
+  it('#stringToBool()', function () {
     expect(stringToBool('true')).to.be.true
+  })
+
+  it('#listToArray()', function () {
+    expect(listToArray('1,2,3,4,5')).to.eql(['1', '2', '3', '4', '5'])
   })
 })
