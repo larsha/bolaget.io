ce329814e715121b99f88d3e42920a372055eb72
Fredrik Lack <fredrik@fre.la>
2018-02-10 12:45:42 +0100

    Fixing typo in deploy script

--- devops/script/deploy.sh
+++ devops/script/deploy.sh
@@ -40,5 +40,5 @@ gcloud docker -- push $NGINX_IMAGE:$COMMIT
 gcloud docker -- push $NGINX_IMAGE:latest
 
 # Deploy
-kubectl -n ${K8S_NAMESPACE} set image deployment/${K8S_DEPLOYMENT_NAME_NGINX} ${K8S_DEPLOYMENT_NAME_NGINX}=$NGINX_IMAGE:$COMMIT
-kubectl -n ${K8S_NAMESPACE} set image deployment/${K8S_DEPLOYMENT_NAME_WEB} ${K8S_DEPLOYMENT_NAME_WEB}=$WEB_IMAGE:$COMMIT
+kubectl -n ${K8S_NAMESPACE} set image deployment/${K8S_DEPLOYMENT_NAME_NGINX} nginx=$NGINX_IMAGE:$COMMIT
+kubectl -n ${K8S_NAMESPACE} set image deployment/${K8S_DEPLOYMENT_NAME_WEB} web=$WEB_IMAGE:$COMMIT
