cf3dd18fa0709cf60f865632f2ad0137fecba1e3
Fredrik Lack <fredrik@fre.la>
2018-05-21 23:06:19 +0200

    Using network for travis test again

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+# Create test network
+docker network create test
+
 # Start ElasticSearch
 docker run \
-  -p 127.0.0.1:9200:9200 \
   -e ES_PLUGINS_INSTALL=analysis-icu \
   --name elasticsearch \
-  --net=host \
+  --network test \
   --rm \
   -d \
   quay.io/pires/docker-elasticsearch-kubernetes:6.1.2
@@ -16,14 +18,14 @@ sleep 20
 
 # Start Node.js
 docker run \
-  -p 3000:3000 \
   -e NODE_ENV=development \
-  -e ELASTIC_HOST=http://127.0.0.1:9200 \
+  -e ELASTIC_HOST=http://elasticsearch:9200 \
   -e ELASTIC_INDEX_SHARDS=1 \
   -e ELASTIC_INDEX_REPLICAS=0 \
   -e ELASTIC_REQUEST_TIMEOUT=15000 \
   -e PORT=3000 \
   -e SYSTEM_PORT=3001 \
+  --network test \
   --rm \
   web \
   npm test
