16dd51fbc00e83db7f91137b32bba2a6f5765c22
Fredrik Lack <fredrik@fre.la>
2018-05-21 23:05:10 +0200

    Travis test script fixes

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -4,9 +4,10 @@ set -e
 
 # Start ElasticSearch
 docker run \
-  -p 9200:9200 \
+  -p 127.0.0.1:9200:9200 \
   -e ES_PLUGINS_INSTALL=analysis-icu \
   --name elasticsearch \
+  --net=host \
   --rm \
   -d \
   quay.io/pires/docker-elasticsearch-kubernetes:6.1.2
@@ -17,7 +18,7 @@ sleep 20
 docker run \
   -p 3000:3000 \
   -e NODE_ENV=development \
-  -e ELASTIC_HOST=http://localhost:9200 \
+  -e ELASTIC_HOST=http://127.0.0.1:9200 \
   -e ELASTIC_INDEX_SHARDS=1 \
   -e ELASTIC_INDEX_REPLICAS=0 \
   -e ELASTIC_REQUEST_TIMEOUT=15000 \
