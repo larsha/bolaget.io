f0987dba225aa7dae060af0de33f7d6a0bb22b77
Fredrik Lack <fredrik@fre.la>
2018-10-31 21:17:59 +0100

    Refactoring travis pipeline

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,17 +2,35 @@
 
 set -e
 
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_NAME
+gcloud --quiet config set container/cluster $CLUSTER_NAME
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+
+WEB_IMAGE=eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}
+NGINX_IMAGE=eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_NGINX}
+
 # Build web
 docker build \
   -t web \
-  -t eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}:$COMMIT \
-  -t eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}:latest \
+  -t $WEB_IMAGE:$COMMIT \
+  -t $WEB_IMAGE:latest \
   -f services/web/Dockerfile services/web
 
 # Build nginx
 docker build \
   --build-arg CONF=production.conf \
   -t nginx \
-  -t eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_NGINX}:$COMMIT \
-  -t eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_NGINX}:latest \
+  -t $NGINX_IMAGE:$COMMIT \
+  -t $NGINX_IMAGE:latest \
   -f services/nginx/Dockerfile services/nginx
+
+# Push images
+gcloud auth configure-docker --quiet
+docker push $WEB_IMAGE:$COMMIT
+docker push $WEB_IMAGE:latest
+docker push $NGINX_IMAGE:$COMMIT
+docker push $NGINX_IMAGE:latest
--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -6,16 +6,7 @@ set -e
 curl -0 https://storage.googleapis.com/cloud-sdk-release/google-cloud-sdk-159.0.0-linux-x86_64.tar.gz | tar -zx -C ${HOME}
 ${HOME}/google-cloud-sdk/install.sh
 source ${HOME}/google-cloud-sdk/path.bash.inc
-
 gcloud --quiet components install kubectl
-
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
-gcloud --quiet config set project $PROJECT_NAME
-gcloud --quiet config set container/cluster $CLUSTER_NAME
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
 # Install Helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
@@ -23,14 +14,13 @@ chmod 700 get_helm.sh
 sudo ./get_helm.sh
 helm init --client-only
 
-# Push images
-gcloud docker -- push $WEB_IMAGE:$COMMIT
-gcloud docker -- push $WEB_IMAGE:latest
-gcloud docker -- push $NGINX_IMAGE:$COMMIT
-gcloud docker -- push $NGINX_IMAGE:latest
-
 # Deploy
 kubectl config set-context $(kubectl config current-context) --namespace=bolagetio
+
+helm dependency build \
+  --tiller-namespace tiller \
+  ./chart
+
 helm upgrade \
   --tiller-namespace tiller \
   --namespace bolagetio \
