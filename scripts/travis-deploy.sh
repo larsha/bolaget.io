87f0d017be28ad58ffe9158e11c470cc15d15afc
Fredrik Lack <fredrik@sthlm.io>
2020-02-26 21:37:31 +0100

    Adding gcloud step in travis

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Download and install Google Cloud SDK
-curl -0 https://storage.googleapis.com/cloud-sdk-release/google-cloud-sdk-223.0.0-linux-x86_64.tar.gz | tar -zx -C ${HOME}
-${HOME}/google-cloud-sdk/install.sh
-source ${HOME}/google-cloud-sdk/path.bash.inc
 gcloud --quiet components install kubectl
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
@@ -24,7 +20,6 @@ helm init \
 	--service-account tiller
 
 # Push images
-gcloud auth configure-docker --quiet
 docker push eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}:$COMMIT
 docker push eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}:latest
 docker push eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/elasticsearch-oss:${ELASTICSEARCH_VERSION}
